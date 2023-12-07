36a492d73093c2b89f8fe74ccd906aa1f5c1b008
rifqifarelmuhammad <rifqifarel222@gmail.com>
2023-12-07 15:52:25 +0700

    improve: added traveler name to mendapatkanSemuaWorldPost response

--- src/repository/world-post.repository.ts
+++ src/repository/world-post.repository.ts
@@ -29,6 +29,16 @@ export class WorldPostRepository {
       where: {
         parentPostId: null,
       },
+      include: {
+        traveler: {
+          select: {
+            nama: true
+          }
+        }
+      },
+      orderBy: {
+        timestamp: 'desc'
+      }
     })
 
     return worldPosts
