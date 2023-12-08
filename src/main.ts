f63f16b4adf6b1ba3cabb3977222562d09104455
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-12-08 22:45:02 +0700

    setup https

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe, Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as fs from 'fs'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const https = process.env.APP_OPEN_HTTPS
+  console.log(https)
+  const httpsOptions = {
+    key: fs.readFileSync(process.env.APP_KEY_PERM),
+    cert: fs.readFileSync(process.env.APP_CERT_PERM),
+  }
+  console.log(httpsOptions)
+  const app = await NestFactory.create(AppModule, { httpsOptions })
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
     origin: process.env.APP_ALLOWED_ORIGIN,
     credentials: true,
   })
-  await app.listen(process.env.APP_PORT ?? 3000)
+
+  if (https) await app.listen(443)
+  else await app.listen(process.env.APP_PORT ?? 3000)
+
+  Logger.log(`Listening on ${await app.getUrl()}`)
 }
 bootstrap()
