cf4d2444b503edba11fc47e8ac42f727c39eb17c
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2023-12-08 23:46:30 +0700

    refactor main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,23 @@ import * as fs from 'fs'
 
 async function bootstrap() {
   const https = process.env.APP_OPEN_HTTPS
-  const httpsOptions = {
-    key: fs.readFileSync(process.env.APP_KEY),
-    cert: fs.readFileSync(process.env.APP_CERT),
-  }
-  const app = await NestFactory.create(AppModule, { httpsOptions })
+  const httpsOptions = https
+    ? {
+        key: fs.readFileSync(process.env.APP_KEY),
+        cert: fs.readFileSync(process.env.APP_CERT),
+      }
+    : {}
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  })
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
     origin: process.env.APP_ALLOWED_ORIGIN,
     credentials: true,
   })
 
-  if (https) await app.listen(443)
-  else await app.listen(process.env.APP_PORT ?? 3000)
+  const appPort = https ? 443 : process.env.APP_PORT
+  await app.listen(appPort ?? 3000)
 
   Logger.log(`Listening on ${await app.getUrl()}`)
 }
